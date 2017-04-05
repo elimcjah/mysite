0225a81837eababe4018d4e346e9f303c86821b6
EliMcJah <Elijah@Fungry.com>
2017-04-04 22:06:47 -0600

    Added the pyramids back on the canvas with a white background

--- public/js/pyramids.js
+++ public/js/pyramids.js
@@ -1,58 +1,88 @@
-var scene, camera, renderer;
-var pyramidGeometry, material, mesh;
+var scene, camera, renderer, material, controls;
+var pyramidGeometry;
 var THREE;
 
 init();
 animate();
+addPyramids();
+render();
 
-function init () {
+function addPyramids () {
+	var xDistance = 50;
+	var zDistance = 30;
+	pyramidGeometry = new THREE.CylinderGeometry(0, 0.8, 2, 4, false);
+	for (var t = 0; t < pyramidGeometry.faces.length; t++) {
+		pyramidGeometry.faces[0].color = new THREE.Color(0x3bc6b6);
+		pyramidGeometry.faces[1].color = new THREE.Color(0x3bc6b6);
+		pyramidGeometry.faces[2].color = new THREE.Color(0xff5ba5);
+		pyramidGeometry.faces[3].color = new THREE.Color(0xff5ba5);
+		pyramidGeometry.faces[4].color = new THREE.Color(0x9957cd);
+		pyramidGeometry.faces[5].color = new THREE.Color(0x9957cd);
+		pyramidGeometry.faces[6].color = new THREE.Color(0x9957cd);
+		pyramidGeometry.faces[7].color = new THREE.Color(0x9957cd);
+	}
 
-	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xffffff);
+	material = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors, side: THREE.DoubleSide });
 
-	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000);
-	camera.position.set(0, 0, 10);
-	camera.lookAt(scene.position);
-	scene.add(camera);
+	var xOffset = -80;
 
-	pyramidGeometry = new THREE.CylinderGeometry(0, 1.5, 1.5, 4, false);
-	for (let i = 0; i < pyramidGeometry.faces.length; i++) {
-		if(pyramidGeometry.faces[i] instanceof THREE.Face4) {
-			pyramidGeometry.faces[i].vertexColors[0] = new THREE.Color(0xFF0000);
-			if((i % 2) == 0){
-				pyramidGeometry.faces[i].vertexColors[1] = new THREE.Color(0x00FF00);
-				pyramidGeometry.faces[i].vertexColors[2] = new THREE.Color(0x0000FF);
-				} else {
-				pyramidGeometry.faces[i].vertexColors[1] = new THREE.Color(0x0000FF);
-				pyramidGeometry.faces[i].vertexColors[2] = new THREE.Color(0x00FF00);
-				}
-			pyramidGeometry.faces[i].vertexColors[3] = new THREE.Color(0xFF0000);
-			} else {
-			pyramidGeometry.faces[i].vertexColors[0] = new THREE.Color(0xFF0000);
-			pyramidGeometry.faces[i].vertexColors[1] = new THREE.Color(0x00FF00);
-			pyramidGeometry.faces[i].vertexColors[2] = new THREE.Color(0x0000FF);
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 3; j++) {
+			var mesh = new THREE.Mesh(pyramidGeometry, material);
+			mesh.position.x = (xDistance * i) + xOffset;
+			mesh.position.z = (zDistance * j);
+			scene.add(mesh);
 		}
 	}
 
-	material = new THREE.MeshBasicMaterial( { vertexColors:THREE.VertexColors, side:THREE.DoubleSide });
+}
 
-	mesh = new THREE.Mesh( pyramidGeometry, material );
-	scene.add( mesh );
+function init () {
 
-	renderer = new THREE.WebGLRenderer();
-	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer = new THREE.WebGLRenderer({ alpha: true });
+	// renderer.setPixelRatio(window.devicePixelRatio);
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	// Add renderer to page
+	document.body.appendChild(renderer.domElement);
 
-	document.body.appendChild( renderer.domElement );
+	// Create camera.
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+	camera.position.z = 100;
+
+	// Add controls
+	// controls = new THREE.TrackballControls(camera);
+	// controls.addEventListener('change', render);
+
+	// Create scene.
+	scene = new THREE.Scene();
+
+	// Create ambient light and add to scene.
+	var light = new THREE.AmbientLight(0x404040); // soft white light
+	scene.add(light);
+
+	// Create directional light and add to scene.
+	var directionalLight = new THREE.DirectionalLight(0xfffffff);
+	directionalLight.position.set(1, 1, 1).normalize();
+	scene.add(directionalLight);
+
+	// Add listener for window resize.
+	window.addEventListener('resize', onWindowResize, false);
+}
+
+function animate () {
+
+	requestAnimationFrame(animate);
+	// controls.update();
 
 }
 
-function animate() {
+function render () {
+	renderer.render(scene, camera);
+}
 
-	requestAnimationFrame( animate );
-
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.02;
-
-	renderer.render( scene, camera );
-
+function onWindowResize () {
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	// controls.handleResize();
 }
